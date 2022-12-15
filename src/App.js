2a05152150a9a552e0e696292dd9524d328f8792
Chris Tuskan <ctuskan@gmail.com>
2022-12-15 18:48:37 -0500

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  // const [pokemon, setPokemon] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    //   .then(response => response.json())
+    //   .then(allPokemon => setData(allPokemon))
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(allPokemon=>setData(allPokemon.data))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mx-auto col-2">
+      <ul>
+        {data.results ? data.results.map((pokemon,index) => {
+          return (<li key={index}>{pokemon.name}</li>)
+        }): null}
+      </ul>
     </div>
   );
 }
